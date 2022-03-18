b748c7415b97d80870dd7493d80df8184cc7a1ce
Soumyakanta Mohanty <soumyakantam022@gmail.com>
2022-03-18 19:15:47 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter , Routes, Route, Link } from 'react-router-dom'; 
+// import { Switch } from 'react-native-switch';
 import './App.css';
 import { Navbar } from './components'
+import Feedpage from './components/Feedpage';
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/feed" element={<Feedpage/>}/>
+      <Route path="/nav" element={<Navbar/>}/>
+
+    </Routes>
+    </BrowserRouter>
+    {/* <Router>
+    <div>
+    <Link to='/feed'>Feedpage</Link>
+    <Link to='/nav'>Navbar</Link>
     </div>
+    <Route exact path='/feed' component={Feedpage}/>
+    <Route exact path='/nav' component={Navbar}/>
+    </Router> */}
+
+
+    
+        
+    </> 
+        
+    
+      
+      
+      
+    
   );
 }
 
 export default App;
+
+
+{/* <Router>
+    <div>
+      
+    <Link to="/feed">Feedpage</Link><br/>
+      <Link to="/nav">Navbar</Link><br/>
+    </div>
+        <Route exact path='/feed' component={Feedpage}/> 
+        <Route exact path='/nav' component={Navbar}/> 
+
+      </Router> */}
---
+++ src/components/Feedpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Feedpage() {
+  return (
+    <div>Feedpage
+        <h2>Hey you</h2>
+    </div>
+  )
+}
+
+export default Feedpage
